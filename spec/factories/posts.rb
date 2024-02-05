dc83f4dc559c20a1d8c3a8a7089a0131461169bf
petrosb <petrosbompotis@gmail.com>
2024-02-05 21:34:16 +0200

    Add a spec for the Post model's default_scope

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,9 +1,16 @@
-
 FactoryBot.define do
   factory :post do
     title { 'a' * 20 }
     content { 'a' * 20 }
-    user
+    association :user, factory: :user_with_unique_email
     category
   end
 end
+
+FactoryBot.define do
+  factory :user_with_unique_email, class: 'User' do
+    email { Faker::Internet.unique.email }
+    password { 'password' }
+  end
+end
+
