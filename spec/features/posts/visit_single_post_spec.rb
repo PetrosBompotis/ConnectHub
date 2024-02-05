866b89a387b7d35af8395b6cbf4101217901a8ef
Anthony Roussos <79643636+anthonyrouss@users.noreply.github.com>
2024-02-05 14:42:06 +0200

    Add functionality to show a single post (#35)
    * Create a show template for posts
    
    * Add a show action
    Add a show action and query a post to an instance variable.
    
    * Create show.scss file and add CSS
    Create show.scss, add CSS and import it in application.scss.
    
    * Add visit single post spec
    Add a feature spec to test if a user can go to a single post from the home page.

---
+++ spec/features/posts/visit_single_post_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.feature "Visit single post", :type => :feature do
+  let(:user) { create(:user) }
+  let(:post) { create(:post) }
+
+  scenario "User goes to a single post from the home page" do
+    post
+    visit root_path
+    page.find(".single-post-card").click
+    expect(page).to have_selector('body .modal')
+    page.find('.post-content .interested').click
+    
+    expect(page).to have_selector('#single-post-content p', text: post.content)
+  end
+
+end
