929c8971db0007c803ce81b83cc2bc4dc9d79235
Anthony Roussos <79643636+anthonyrouss@users.noreply.github.com>
2024-02-09 23:59:40 +0200

    Create Conversation Models & Update User Associations (#52)
    * Create private conversation model
    
    * Create private message model
    
    * Update user associations
    Define associations between User, private conversation and private message models.
    
    * Update schema.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
+  has_many :private_messages, class_name: 'Private::Message'
+  has_many  :private_conversations, 
+          foreign_key: :sender_id, 
+          class_name: 'Private::Conversation'
+  
   # In User model
   validates_uniqueness_of :email
 
