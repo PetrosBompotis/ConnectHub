51a4457ada433b610b6b777f1fbff870a359369c
petrosb <petrosbompotis@gmail.com>
2024-02-11 17:47:07 +0200

    Create a Contact model and write specs for it

--- db/migrate/20240211153129_create_contacts.rb
+++ db/migrate/20240211153129_create_contacts.rb
@@ -1,6 +1,9 @@
-class CreateContacts < ActiveRecord::Migration[7.1]
+class CreateContacts < ActiveRecord::Migration[5.1]
   def change
     create_table :contacts do |t|
+      t.belongs_to :user, index: true
+      t.belongs_to :contact, index: true
+      t.boolean :accepted, default: false
 
       t.timestamps
     end
