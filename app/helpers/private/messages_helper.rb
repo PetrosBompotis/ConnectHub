eb889d58c4c235667e573eeec831b52652626176
Anthony Roussos <79643636+anthonyrouss@users.noreply.github.com>
2024-02-12 17:54:45 +0200

    Implement Messenger Feature with Responsive Design (#74)
    * Create messengers controller & helper
    On smaller screens, users are going to chat through messenger instead of conversation windows.
    
    * Define routes for messenger actions
    
    * Create an open_messenger.html.erb in the /messengers
    
    * Create a ConversationForMessengerService & specs
    
    * Create a template for the MessengersController's index action
    
    * Create a template for the MessengersController's get_private_conversation action
    
    * Update application.html.erb
    
    * Create spec for the ApplicationHelper
    
    * Create a messenger.scss inside the partials
    
    * Add messenger class
    
    * Define an autoload_messenger_messages in the Shared::MessagesHelper
    
    * Create append_previous_messages_partial_path method
    
    * Add replace_link_to_private_messages_partial_path
    
    * Update _load_more_messages.js.erb
    
    * Create partials
    
    * Create messenger.js
    
    * Add alternative links for smaller devices to open conversations
    
    * Update messenger.js
    Add JavaScript to messenger.js to show a different
    messenger's version on mobile devices.
    
    * Create a get_group_conversation.js.erb template and its partials inside the messengers

--- app/helpers/private/messages_helper.rb
+++ app/helpers/private/messages_helper.rb
@@ -31,5 +31,13 @@ module Private::MessagesHelper
       'private/messages/load_more_messages/window/replace_link_to_messages'
     end
   end
+
+  def replace_link_to_private_messages_partial_path
+    if @is_messenger == 'true'
+      'private/messages/load_more_messages/messenger/replace_link_to_messages'
+    else
+      'private/messages/load_more_messages/window/replace_link_to_messages'
+    end
+  end
   
 end
